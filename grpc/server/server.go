2fa6fe42c88895d5cec4d60b83619d472367ce9b
Lv Yanlong <lvyanlong@senbit.com>
2021-01-28 16:37:59 +0800

    feat: add grpc server

--- grpc/server/server.go
+++ grpc/server/server.go
@@ -1,5 +1,21 @@
 package main
 
-func main(){
-    net.Listen("")
+import (
+	"log"
+	"net"
+
+	pb "github.com/notes/grpc/protos/hello"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
